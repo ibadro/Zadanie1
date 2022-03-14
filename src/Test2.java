953f5105768d97457e232d97b56fdd7a0221fd32
big-new <ilnurb@mail.ru>
2022-03-14 22:58:52 +0500

    111яа cx

--- src/Test2.java
+++ src/Test2.java
@@ -25,11 +25,13 @@ class BankAccount {
             BankAccount YourAcount = new BankAccount();
             BankAccount HisAcount = new BankAccount();
 
-            MyAcount.id =1;
+            MyAcount.id =6;
             MyAcount.name ="zaur";
             MyAcount.balance =1651.2;
 
             System.out.println(MyAcount.id);
+            System.out.println(MyAcount.name);
+            System.out.println(MyAcount.balance);
         }
     }
 
--- src/Test3.java
+++ src/Test3.java
@@ -1,4 +1,4 @@
-public class trest1 {
+public class Test3 {
     public static void main(String[] args) {
 
         byte b1 = 0b1100;
