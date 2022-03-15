213f199c010d78f0e0058f0da7cbf6be9e07af76
i.badretdinov <ilnurb@mail.ru>
2022-03-15 08:33:49 +0500

    xz

--- src/DomZadanie5.java
+++ src/DomZadanie5.java
@@ -1,53 +1,32 @@
-public class DomZadanie5 {
-    class BankAccount {
-        int id =10;
-        String name ="Petr";
-        double balance= 100;
+    class BankAccount1 {
+
+        int id = 10;
+        String name = "Petr";
+        double balance = 100;
 
         void popolnenieSheta(double summaPopolnenya) {
-            System.out.println("Balans do poplnenya: "+ balance);
-            System.out.println("Balans popolnyetcya na: "+ summaPopolnenya);
+            System.out.println("Balans do poplnenya: " + balance);
+            System.out.println("Balans popolnyetcya na: " + summaPopolnenya);
             balance += summaPopolnenya;
-            System.out.println("Balans posle poplnenya: "+ balance);
+            System.out.println("Balans posle poplnenya: " + balance);
             System.out.println();
         }
+
         void snyantieSoSheta(double summaSnytya) {
-            System.out.println("Balans do snyatya: "+ balance);
-            System.out.println("Balans umenshaetsya na: "+ summaPopolnenya);
-            balance -= summaPopolnenya;
-            System.out.println("Balans posle snyatya: "+ balance);
+            System.out.println("Balans do snyatya: " + balance);
+            System.out.println("Balans umenshaetsya na: " + summaSnytya);
+            balance -= summaSnytya;
+            System.out.println("Balans posle snyatya: " + balance);
             System.out.println();
-
-
-            public static void main(String[] args) {
-
-                BankAccount MyAcount = new BankAccount();
-                BankAccount YourAcount = new BankAccount();
-                BankAccount HisAcount = new BankAccount();
-
-                MyAcount.id =1;
-                MyAcount.name ="zaur";
-                MyAcount.balance =1651.2;
-
-//         System.out.println(MyAcount.id);
-            }
         }
+    }
+                    class BankAccounttest1 {
 
-        class BankAccounttest {
+                        public static void main(String[] args) {
+                            BankAccount1 MyAccount = new BankAccount1();
 
-            public static void main(String[] args) {
+                            MyAccount.popolnenieSheta(30.5);
+                            MyAccount.snyantieSoSheta(20.5);
 
-                BankAccount MyAcount = new BankAccount();
-                BankAccount YourAcount = new BankAccount();
-                BankAccount HisAcount = new BankAccount();
-
-                MyAcount.id =6;
-                MyAcount.name ="zaur";
-                MyAcount.balance =1651.2;
-
-                System.out.println(MyAcount.id);
-                System.out.println(MyAcount.name);
-                System.out.println(MyAcount.balance);
-            }
-        }
-}
+                        }
+                    }
