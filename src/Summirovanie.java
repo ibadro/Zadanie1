2de06c24ac635e4d9e6a03c263d7fb2439839e54
big-new <ilnurb@mail.ru>
2022-03-17 21:54:22 +0500

    111яа cx

--- src/Summirovanie.java
+++ src/Summirovanie.java
@@ -4,43 +4,43 @@
 
 
 public class Summirovanie {
-
     int sum() {
-        int result =0;
+        int result = 0;
         System.out.println("Summa vsex shisel:" + result);
         return result;
     }
+
     int sum(int a) {
-        int result1 =a;
+        int result1 = a;
         System.out.println("Summa vsex shisel:" + result1);
         return result1;
     }
-    int sum(int b,int c) {
-        int result2 =b+c;
+
+    int sum(int b, int c) {
+        int result2 = b + c;
         System.out.println("Summa vsex shisel:" + result2);
         return result2;
     }
+
     int sum(int d, int e, int f) {
         int result3 = d + e + f;
         System.out.println("Summa vsex shisel:" + result3);
         return result3;
     }
+
     int sum(int g, int h, int i, int j) {
         int result4 = g + h + i + j;
         System.out.println("Summa vsex shisel:" + result4);
         return result4;
     }
-
-class SummirovanieTest {
-    static void main(String[] args) {
-        Summirovanie s =    new Summirovanie();
-        s.sum();
-        s.sum(3);
-        s.sum(3, 5 ,);
-        s.sum(3, 5,7,);
-        s.sum(3, 5 ,7, 11);
-
-    }
-
-
 }
+    class SummirovanieTest {
+       public static void main(String[] args) {
+            Summirovanie s = new Summirovanie();
+            s.sum();
+            s.sum(3);
+            s.sum(3, 5);
+            s.sum(3, 5, 7);
+            s.sum(3, 5, 7, 11);
+        }
+    }
