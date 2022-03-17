f26f051fba92bb6faea4af48f18d378153035a58
i.badretdinov <ilnurb@mail.ru>
2022-03-17 09:19:00 +0500

    xzям

--- src/MethodOverLoading.java
+++ src/MethodOverLoading.java
@@ -1,4 +1,40 @@
+//написать метод который будет выводить на экран переменную типа данных int +boolean + string
+
 public class MethodOverLoading {
-
-
+//    void showInt(int i1) {
+    void show(int i1) {
+        System.out.println(i1);
+    }
+//        void showBoolean(boolean b1) {
+    void show(boolean b1) {
+        System.out.println(b1);
+    }
+//            void showString(String s1) {
+    void show(String s1) {
+        System.out.println(s1);
+    }
 }
+
+        class MethodOverLoadingTest {
+            public static void main(String[] args) {
+                MethodOverLoading mO = new MethodOverLoading();
+                int a =100;
+//                mO.showInt(a);
+                mO.show(a);
+                boolean b = true;
+//                mO.showBoolean(b);
+                mO.show(b);
+                String s =  "Privet";
+//                mO.showString(s);
+                mO.show(s);
+            }
+
+        }
+
+
+
+
+
+
+
+
