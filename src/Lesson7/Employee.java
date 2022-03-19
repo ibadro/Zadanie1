889097eb3f7426b4cb1cfbfb0853e1b56a328626
big-new <ilnurb@mail.ru>
2022-03-19 15:38:22 +0500

    111яа cxd

---
+++ src/Lesson7/Employee.java
@@ -0,0 +1,16 @@
+package Lesson7;
+
+public class Employee {
+  public  double salary;
+   public void  davoynayaZP() {
+
+        System.out.println("novayaZP="+ salary*2);
+    }
+    Employee(double salary2) {
+        salary = salary2
+    }
+
+    public static void main(String[] args) {
+        Employee emp =  new Employee();
+    }
+}
