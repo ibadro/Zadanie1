f7becdc768cb3f1e61231a37652e4863f55d0f35
big-new <ilnurb@mail.ru>
2022-03-21 21:23:17 +0500

    111яа cxd

---
+++ src/lesson8_____/Zadacha1.java
@@ -0,0 +1,23 @@
+package lesson8_____;
+
+//  В первом кклассе создайте 2 static метода. 1ый пусто умножает число из парамтрев ввода и возвращает их проиведение 2ой - делит первое число на второе и нечего не возвращает,
+// лишь выводит дисплей в читабельном виде целое число и остаток. Во втором классе дважды используйте данные методы.
+public class Zadacha1 {
+    static double umnojenie(double a, double b, double c) {
+        return a * b * c;
+    }
+
+    static void delenie(int a, int b) {
+        System.out.println("Целое частное = " + a / b + ". Остаток = " + a % b);
+    }
+}
+
+class Zadacha1Test {
+
+    public static void main(String[] args) {
+        System.out.println(Zadacha1.umnojenie(2, 23., 3));
+        System.out.println(Zadacha1.umnojenie(2, 26., 1232));
+        Zadacha1.delenie(10, 5);
+        Zadacha1.delenie(333, 232);
+    }
+}
---
+++ src/lesson8_____/Zadani2.java
@@ -0,0 +1,31 @@
+package lesson8_____;
+
+public class Zadani2 {
+
+    public final static double PI = 3.14;
+
+    public double ploshad(double radius) {
+        double p1 = PI * radius * radius;
+        return p1;
+    }
+
+    public static double dlina(double radius2) {
+        double d1 = 2 * PI * radius2;
+        return d1;
+    }
+    public void info(double radius3){
+        System.out.println("Радиус = " + radius3);
+        System.out.println("Площадь круга = " + ploshad(radius3));
+        System.out.println("Длина окружности = " + dlina(radius3));
+    }
+
+}
+
+class Test22{
+    public static void main(String[] args) {
+        Zadani2 z = new Zadani2();
+        z.ploshad(5);
+        Zadani2.dlina(7.5);
+        z.info(3);
+    }
+}
