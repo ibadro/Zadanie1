7699531e5d63552606579aa7f18c0c1819d807ff
i.badretdinov <ilnurb@mail.ru>
2022-03-21 17:25:55 +0500

    xzямс

--- src/lesson8_____/Car.java
+++ src/lesson8_____/Car.java
@@ -1,7 +1,15 @@
 package lesson8_____;
-
+//Урок 8 - Модификаторы «final» и «static». Понятие «константа»
 public class Car {
-
-
-
+    String color = "blue";
+    String engine = "V6";
 }
+    class Human {
+        String name = "ivan";
+        final Car c = new Car();
+        public static void main(String[] args) {
+            Human h1 = new Human ();
+            h1.c.engine= "V8";
+        }
+     }
+
