8f1b53cd1ced4d6dbb3144a2f6121dbda0c091b9
big-new <ilnurb@mail.ru>
2022-03-24 21:25:43 +0500

    111яа cxd

---
+++ src/Lesson14/Test1.java
@@ -0,0 +1,11 @@
+package Lesson14;
+// Циклы в JAVA
+public class Test1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 21; i++) {
+            System.out.println(" urok № 14");
+            System.out.println(" privet");
+
+        }
+    }
+}
---
+++ src/Lesson14/Test2.java
@@ -0,0 +1,9 @@
+package Lesson14;
+
+public class Test2 {
+    public static void main(String[] args) {
+        for (int i= 5; i < 11 && i> -3 || i>4; i++) {
+            System.out.println(i);
+        }
+    }
+}
