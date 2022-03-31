32ad6fd1aec3f7e1a568d80d643634210327ab2c
i.badretdinov <ilnurb@mail.ru>
2022-03-31 20:07:55 +0500

    xzямс

--- src/Lesson14/Test1.java
+++ src/Lesson14/Test1.java
@@ -1,12 +1,12 @@
 package Lesson14;
+
 // Циклы в JAVA
 public class Test1 {
     public static void main(String[] args) {
-        for (int i = 1; ; i++) {
-            //System.out.println(" urok № 14");
-           // System.out.println(" privet");
+        for (int i = 1; i <= 10; i++) { // выводить на экран пока I  меньше или равен 10
+            System.out.println(" urok № 14");
+            System.out.println(" privet");
             System.out.println(i);
-
         }
     }
 }
--- src/Lesson14/Test6.java
+++ src/Lesson14/Test6.java
@@ -19,3 +19,17 @@ public class Test6 {
         t.time();
     }
 }
+
+class Test6134143 {
+    public void time (){
+        for (int chas=0; chas <=23; chas++ ){
+            for (int minuta=0; minuta<=59; minuta++){
+                System.out.println(chas+ ":" + minuta);
+            }
+        }
+     }
+    public static void main(String[] args) {
+        Test6134143 tе =new Test6134143();
+        tе.time();
+    }
+}
