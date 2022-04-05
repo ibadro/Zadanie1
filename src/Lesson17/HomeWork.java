5b2c0dc779b96a9100d3edf06e32ac3e455c44b4
i.badretdinov <ilnurb@mail.ru>
2022-04-05 19:42:09 +0500

    xzямс

--- src/Lesson17/HomeWork.java
+++ src/Lesson17/HomeWork.java
@@ -4,5 +4,31 @@ package Lesson17;
 // Продемонстрируйте данный метод.
 
 public class HomeWork {
+    public static boolean ravenstvo (StringBuilder sb1, StringBuilder sb2){
+        boolean result =true;
+        if (sb1.length()== sb2.length()) {
+            for (int i=0; i<sb1.length(); i++){
+                if(sb1.charAt(i) !=sb2.charAt(i)){
+                    result =false;
+                    break;
+                }
+            }
+        }else {
+            result=false;
+        }
+        return result;
+    }
+}
+class HomeWorkTest {
+    public static void main(String[] args) {
+        StringBuilder sb3 = new StringBuilder("hello");
+        StringBuilder sb4 = new StringBuilder("hello");
+        StringBuilder sb5 = new StringBuilder("hello");
+        boolean a = HomeWork.ravenstvo(sb3,sb4);
+        System.out.println(a);
+        System.out.println(HomeWork.ravenstvo(sb4,sb5));
+        System.out.println(HomeWork.ravenstvo( new StringBuilder(""), new StringBuilder("")));
+        System.out.println(HomeWork.ravenstvo( new StringBuilder(), new StringBuilder()));
 
+    }
 }
