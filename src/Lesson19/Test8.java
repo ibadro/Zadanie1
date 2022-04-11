287e7f91ac48819d26a9be25ecbca14608337012
i.badretdinov <ilnurb@mail.ru>
2022-04-11 10:09:26 +0500

    xzямс

--- src/Lesson19/Test8.java
+++ src/Lesson19/Test8.java
@@ -13,3 +13,16 @@ public class Test8 {
         }
     }
 }
+ class Test81{
+     public static void main(String[] args) {
+
+         String[] array = {"privet", "poka", "ok"};
+         for (String s : array) {
+             s = new String("hello");
+         }
+
+         for (int i = 0; i < array.length; i++) {
+             System.out.print(array[i] + "");
+         }
+     }
+}
