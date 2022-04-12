1518a3ac2b7433a73991c68eff1dd6819850c024
big-new <ilnurb@mail.ru>
2022-04-12 21:29:19 +0500

    111яа cxd

--- src/corejava/Cipher.java
+++
@@ -1,8 +0,0 @@
-package corejava;
-
-public interface Cipher
-{
-   byte[] encrypt(byte[] source, byte[] key);
-   byte[] decrypt(byte[] source, byte[] key);
-   int strength();
-}
--- src/corejava/GreeterService.java
+++
@@ -1,9 +0,0 @@
-package corejava;
-
-import java.util.Locale;
-
-public interface GreeterService 
-{
-    String greet(String subject);
-    Locale getLocale();
-}
